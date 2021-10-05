59828d901af3379991529fe0b43999c5939beb15
joshua-kyle1 <75819392+joshua-kyle1@users.noreply.github.com>
2021-10-05 12:12:25 +0800

    changed title vol

--- volunteer/src/App.vue
+++ volunteer/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Volunteer App"/>
+    <HelloWorld msg="Volunteer"/>
   </div>
 </template>
 
