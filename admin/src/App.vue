82c4ea7ae9dde2014ef8447a53a8beb31e2960c0
Al Christian Gochangco <71010271+gotchi100@users.noreply.github.com>
2021-10-05 12:08:26 +0800

    Change Admin Title

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Aral Pinoy"/>
+    <HelloWorld msg="Admin Title"/>
   </div>
 </template>
 
