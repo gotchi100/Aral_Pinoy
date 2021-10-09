680c0e06e23f14f0595e434110a77edbd298139e
Al Christian Gochangco <71010271+gotchi100@users.noreply.github.com>
2021-10-09 14:52:27 +0800

    Update Admin Login and Registration Form

---
+++ admin/src/components/Registration_Page.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Navbar />
+        <RegistrationForm/>    
+        </div>
+</template>
+
+<script>
+import RegistrationForm from './Registration_Form.vue'
+import Navbar from './Navbar.vue'
+
+export default ({
+  name: 'Registration_Page',
+  components: {
+    RegistrationForm,
+    Navbar
+  }
+})
+</script>
