227db4832fe3dfae7f04885cba81e0a486a06c8b
Aral Pinoy <aral_pinoy_captstone@outlook.com>
2021-10-14 00:28:36 +0800

    Implement routes for creating and retrieving users

--- api/models/users.js
+++ api/models/users.js
@@ -3,8 +3,25 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    contactNumber: String,
+    firstName: {
+        type: String,
+        required: true
+    },
+    middleName: String,
+    lastName: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,18 +5,55 @@ const UserModel = require('../models/users');
 /* Create a user. */
 router.post('/', async function(req, res, next) {
   const {
+    email,
+    password,
+    contactNumber,
     firstName,
+    middleName,
     lastName
   } = req.body;
 
   const user = new UserModel();
 
+  user.email = email;
+  user.password = password;
+  user.contactNumber = contactNumber;
   user.firstName = firstName;
+  user.middleName = middleName;
   user.lastName = lastName;
 
-  await user.save();
+  try {
+    await user.save();
 
-  res.send(user);
+    res.send(user);
+  } catch (error) {
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+
+    next();
+  }
+});
+
+/* Retrieve a user. */
+router.get('/:id', async function(req, res, next) {
+  const { id } = req.params;
+
+  const user = await UserModel.findById(id);
+
+  try {
+    res.send(user);
+  } catch (error) {
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+
+    next();
+  }
 });
 
 module.exports = router;
