9739ffa7ea008117a911ce168235dd81152d36fe
Aral Pinoy <aral_pinoy_captstone@outlook.com>
2021-11-03 18:54:28 +0800

    Integrate user list API endpoint to volunteer list

--- admin/src/pages/AdminVolunteerList.vue
+++ admin/src/pages/AdminVolunteerList.vue
@@ -1,173 +1,142 @@
 <template>
-<div class="volunteerlist">
-  <b-card class="card" style="display: inline-block; height: 100%; overflow: auto; width: 1300px; border-radius: 20px; margin-top:40px;">
-  <b-container fluid>
-    <!-- User Interface controls -->
-    <h1 style="font-family:'Bebas Neue', cursive;">
-        Volunteers
-    </h1>
-    <b-row>
-      <b-container class="bv-example-row">
+  <div class="volunteerlist">
+    <b-card class="card" style="display: inline-block; height: 100%; overflow: auto; width: 1300px; border-radius: 20px; margin-top:40px;">
+      <b-container fluid>
+        <!-- User Interface controls -->
         <b-row>
-          <b-col>
-            <b-col sm="5" md="6" class="my-1">
-              <b-form-group style="font-size: 15px; font-family:'Bebas Neue', cursive;"
-                label="Per page"
-                label-for="per-page-select"
-                label-cols-sm="6"
-                label-cols-md="4"
-                label-cols-lg="3"
-                label-align-sm="right"
-                label-size="sm"
-                class="mb-0"
-              >
-                <b-form-select
-                  id="per-page-select"
-                  v-model="perPage"
-                  :options="pageOptions"
-                  size="sm"
-                ></b-form-select>
-              </b-form-group>
-            </b-col>
-          </b-col>
-          <b-col>
-          </b-col>
-          <b-col>
-            <br>
-            <b-input-group size="sm">
-              <p style="font-size: 20px; font-family:'Bebas Neue', cursive;">Search &nbsp; &nbsp; </p>
-              <b-form-input
-                id="filter-input"
-                v-model="filter"
-                type="search"
-                placeholder="Type to Search" style="height:30px; width:300px; border-radius: 10px;"
-              ></b-form-input>
-            </b-input-group>
-            <br>
+          <b-col cols="12">
+            <h1 style="font-family:'Bebas Neue', cursive;">
+              Volunteers
+            </h1>
           </b-col>
         </b-row>
+
+        <b-row>
+          <b-col cols="12">
+            <b-container>
+              <b-row>
+                <b-col cols="4">
+                  <b-form-group
+                    style="font-size: 15px; font-family:'Bebas Neue', cursive;"
+                    label="Per page"
+                    label-for="per-page-select"
+                    content-cols="12"
+                  >
+                    <b-form-select
+                      id="per-page-select"
+                      class="w-25"
+                      v-model="perPage"
+                      :options="pageOptions"
+                      @change="onPerPageChange"
+                    ></b-form-select>
+                  </b-form-group>
+                </b-col>
+                <!-- TODO: Search by email or full name -->
+                <!-- <b-col cols="4">
+                  <br>
+                  <b-input-group size="sm">
+                    <p style="font-size: 20px; font-family:'Bebas Neue', cursive;">Search &nbsp; &nbsp; </p>
+                    <b-form-input
+                      id="filter-input"
+                      v-model="filter"
+                      type="search"
+                      placeholder="Type to Search" style="height:30px; width:300px; border-radius: 10px;"
+                    ></b-form-input>
+                  </b-input-group>
+                  <br>
+                </b-col> -->
+              </b-row>
+            </b-container>
+          </b-col>
+        </b-row>
+
+        <!-- Main table element -->
+        <b-row class="pt-4">
+          <b-col cols="12">
+            <b-table
+              :items="getUsers"
+              :fields="fields"
+              :current-page="currentPage"
+              :per-page="perPage"
+              stacked="md"
+              style="background:white"
+              show-empty
+              small
+              primary-key="_id"
+            >
+              <template #cell(email)="row">
+                <b-link :to="`/view-volunteer-profile/${row.item._id}`">{{ row.value }}</b-link>
+              </template>
+
+              <template #cell(name)="row">
+                {{ row.item.firstName }} {{ row.item.lastName }}
+              </template>
+            </b-table>
+          </b-col>
+        </b-row>
+
+        <b-row class="pt-4">
+          <b-col cols="12">
+            <b-pagination
+              v-model="currentPage"
+              :total-rows="total"
+              :per-page="perPage"
+              align="center"
+              @change="onPaginationChange"
+            ></b-pagination>
+          </b-col>
+          <b-col></b-col>
+        </b-row>
       </b-container>
-    </b-row>
-
-    <!-- Main table element -->
-    <b-table
-      :items="items"
-      :fields="fields"
-      :current-page="currentPage"
-      :per-page="perPage"
-      :filter="filter"
-      :filter-included-fields="filterOn"
-      :sort-by.sync="sortBy"
-      :sort-desc.sync="sortDesc"
-      :sort-direction="sortDirection"
-      stacked="md"
-      show-empty
-      small
-      @filtered="onFiltered"
-      style="background:white"
-    >
-      <template #cell(email)="row">
-        <b-link :to="`/view-volunteer-profile/${row.index}`">{{ row.value }}</b-link>
-      </template>
-
-      <template #cell(name)="row">
-        {{ row.item.firstName }} {{ row.item.lastName }}
-      </template>
-    </b-table>
-  <b-row>
-    <b-col></b-col>
-    <b-col>
-        <b-col class="my-1">
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="totalRows"
-            :per-page="perPage"
-            align="fill"
-            size="sm"
-            class="my-0"
-          ></b-pagination>
-        </b-col>
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-  </b-container>
-</b-card>
-</div>
+    </b-card>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+const axios = require('axios').default
+
 export default {
+  name: 'VolunteerList',
   data () {
     return {
-      items: [
-        { email: 'Macdonald@gmail.com', firstName: 'Dickerson', lastName: 'Macdonald', contactNumber: '091857489714' },
-        { email: 'Shaw@gmail.com', firstName: 'Larsen', lastName: 'Shaw', contactNumber: '090857475614' },
-        { email: 'Navarro@gmail.com', firstName: 'Mini', lastName: 'Navarro', contactNumber: '098857489714' },
-        { email: 'Wilson@gmail.com', firstName: 'Geneva', lastName: 'Wilson', contactNumber: '048857489712' },
-        { email: 'Carney@gmail.com', firstName: 'Jami', lastName: 'Carney', contactNumber: '068857489712' },
-        { email: 'Dunlap@gmail.com', firstName: 'Essie', lastName: 'Dunlap', contactNumber: '018857489712' },
-        { email: 'Macdonald@gmail.com', firstName: 'Thor', lastName: 'Macdonald', contactNumber: '088857489712' },
-        { email: 'Navarro@gmail.com', firstName: 'Mitzi', lastName: 'Navarro', contactNumber: '098857689712' },
-        { email: 'Wilson@gmail.com', firstName: 'Genevieve', lastName: 'Wilson', contactNumber: '098837489712' },
-        { email: 'Carney@gmail.com', firstName: 'John', lastName: 'Carney', contactNumber: '096857489712' },
-        { email: 'Dunlap@gmail.com', firstName: 'Dick', lastName: 'Dunlap', contactNumber: '099857489712' },
-        { email: 'Macdonald@gmail.com', firstName: 'Dickerson', lastName: 'Macdonald', contactNumber: '091857489714' },
-        { email: 'Shaw@gmail.com', firstName: 'Larsen', lastName: 'Shaw', contactNumber: '090857475614' },
-        { email: 'Navarro@gmail.com', firstName: 'Mini', lastName: 'Navarro', contactNumber: '098857489714' },
-        { email: 'Wilson@gmail.com', firstName: 'Geneva', lastName: 'Wilson', contactNumber: '048857489712' },
-        { email: 'Carney@gmail.com', firstName: 'Jami', lastName: 'Carney', contactNumber: '068857489712' },
-        { email: 'Dunlap@gmail.com', firstName: 'Essie', lastName: 'Dunlap', contactNumber: '018857489712' },
-        { email: 'Macdonald@gmail.com', firstName: 'Thor', lastName: 'Macdonald', contactNumber: '088857489712' },
-        { email: 'Navarro@gmail.com', firstName: 'Mitzi', lastName: 'Navarro', contactNumber: '098857689712' },
-        { email: 'Wilson@gmail.com', firstName: 'Genevieve', lastName: 'Wilson', contactNumber: '098837489712' },
-        { email: 'Carney@gmail.com', firstName: 'John', lastName: 'Carney', contactNumber: '096857489712' },
-        { email: 'Dunlap@gmail.com', firstName: 'Dick', lastName: 'Dunlap', contactNumber: '099857489712' }
-      ],
-      fields: [
-        { key: 'email', label: 'Email', sortable: true, class: 'text-center' },
-        { key: 'name', label: 'Full Name', sortable: true, sortDirection: 'desc' },
-        { key: 'contactNumber', label: 'Contact Number', sortable: true, class: 'text-center' }
-      ],
-      totalRows: 1,
+      users: [],
+      total: 0,
       currentPage: 1,
       perPage: 5,
-      pageOptions: [5, 10, { value: 100, text: 'Show a lot' }],
-      sortBy: '',
-      sortDesc: false,
-      sortDirection: 'asc',
-      filter: null,
-      filterOn: [],
-      infoModal: {
-        id: 'info-modal',
-        title: '',
-        content: ''
-      }
+      pageOptions: [5, 10, 20],
+      fields: [
+        { key: 'email', label: 'Email', class: 'text-center' },
+        { key: 'name', label: 'Full Name', sortDirection: 'desc' },
+        { key: 'contactNumber', label: 'Contact Number', class: 'text-center' }
+      ]
     }
   },
   computed: {
-    sortOption () {
-      // Create an options list from our fields
-      return this.fields
-        .filter(f => f.sortable)
-        .map(f => {
-          return { text: f.label, value: f.key }
-        })
+    ...mapGetters(['token']),
+    pageOffset () {
+      return (this.currentPage - 1) * this.perPage
     }
   },
-  mounted () {
-    // Set the initial number of items
-    this.totalRows = this.items.length
-  },
   methods: {
-    info (item, index, button) {
-      this.infoModal.title = `Row index: ${index}`
-      this.infoModal.content = JSON.stringify(item, null, 2)
-      this.$root.$emit('bv::show::modal', this.infoModal.id, button)
-    },
-    onFiltered (filteredItems) {
-      // Trigger pagination to update the number of buttons/pages due to filtering
-      this.totalRows = filteredItems.length
-      this.currentPage = 1
+    async getUsers (ctx) {
+      const queryString = new URLSearchParams()
+
+      queryString.set('limit', this.perPage)
+      queryString.set('offset', this.pageOffset)
+
+      const { data } = await axios.get(`http://localhost:3000/users?filters.roles[]=volunteer&${queryString.toString()}`, {
+        headers: {
+          Authorization: `Bearer ${this.token}`
+        }
+      })
+
+      const { results, total } = data
+
+      this.total = total
+
+      return results
     }
   }
 }
@@ -175,20 +144,20 @@ export default {
 
 <style scoped>
 .volunteerlist {
-position: relative;
+  position: relative;
 }
 .volunteerlist:before {
-background-image: url('https://rs.projects-abroad.ie/v1/hero/product-5b5b2f57d7d1b.[1600].jpeg');
-content: ' ';
-display: block;
-position: absolute;
-left: 0;
-top: 0;
-width: 100%;
-height: 100%;
-opacity: 0.4;
-background-repeat: no-repeat;
-background-size: cover;
-padding-top: 695px;
+  background-image: url('https://rs.projects-abroad.ie/v1/hero/product-5b5b2f57d7d1b.[1600].jpeg');
+  content: ' ';
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.4;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding-top: 695px;
 }
 </style>
