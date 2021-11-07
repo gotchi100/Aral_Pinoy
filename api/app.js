2cc4d039d0f732388150ba028aaa4f56d20d0a80
Aral Pinoy <aral_pinoy_captstone@outlook.com>
2021-11-07 15:55:27 +0800

    Implement MRC pattern

--- api/app.js
+++ api/app.js
@@ -10,9 +10,8 @@ const jwtMiddleware = require('express-jwt')
 
 const config = require('./config')
 
-const indexRouter = require('./routes/index')
+const mainRouter = require('./routes/main')
 const usersRouter = require('./routes/users')
-const rolesRouter = require('./routes/roles')
 const adminRouter = require('./routes/admin')
 
 const logger = debug('api:server')
@@ -43,9 +42,8 @@ app.use(
   })
 )
 
-app.use('/', indexRouter)
+app.use('/', mainRouter)
 app.use('/users', usersRouter)
-app.use('/roles', rolesRouter)
 app.use('/admin', adminRouter)
 
 app.use(function (err, req, res, next) {
