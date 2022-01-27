551ea4ec015e135cb4c696e0ff0ed7107799a943
Aral Pinoy <aralpinoycapstone@gmail.com>
2022-01-28 04:19:25 +0800

    Add validation to event creation page

--- api/routes/events.js
+++ api/routes/events.js
@@ -31,8 +31,8 @@ const contactsSchema = Joi.array().items(
 
 const jobsSchema = Joi.array().items(
   Joi.object({
-    name: Joi.string().trim().max(255).required(),
-    description: Joi.string().trim().empty('').max(200),
+    name: Joi.string().trim().max(50).required(),
+    description: Joi.string().trim().empty('').max(100),
     requirements: Joi.object({
       max: Joi.number().min(1).default(1)
     }).required(),
@@ -48,7 +48,7 @@ const questionsSchema = Joi.array().items(
 )
 
 const createEventValidator = Joi.object({
-  name: Joi.string().trim().max(255).required(),
+  name: Joi.string().trim().max(100).required(),
   description: Joi.string().trim().empty('').max(5000),
   date: Joi.object({
     start: Joi.date().iso().required(),
